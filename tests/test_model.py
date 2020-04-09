08b333d57f14765fcd0b1f8b9331626f695ea2f6
Hector Cortes <hector.cortes@outlook.com>
2020-04-09 02:21:23 -0700

    integrating travis-ci

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,32 +4,30 @@ from modelbuilder.model import Model, Attribute
 
 
 class TestPrimitiveModel(unittest.TestCase):
-    def test_model_creation(self):
+    def setUp(self):
         class Primitive(Model):
             string = Attribute(obj_name="string", obj_type=str)
             _float = Attribute(obj_name="float", obj_type=float)
             integer = Attribute(obj_name="integer", obj_type=int)
             boolean = Attribute(obj_name="boolean", obj_type=bool)
 
-        test_class = Primitive()
+        self.Primitive = Primitive
+
+    def test_model_init_none(self):
+        test_class = self.Primitive()
+
         self.assertIsNone(test_class.string)
         self.assertIsNone(test_class._float)
         self.assertIsNone(test_class.integer)
         self.assertIsNone(test_class.boolean)
 
-    def test_model_setget(self):
-        class Primitive(Model):
-            string = Attribute(obj_name="string", obj_type=str)
-            _float = Attribute(obj_name="float", obj_type=float)
-            integer = Attribute(obj_name="integer", obj_type=int)
-            boolean = Attribute(obj_name="boolean", obj_type=bool)
-
+    def test_model_init(self):
         test_string = "Hello"
         test_float = 1.0
         test_integer = 5
         test_boolean = True
 
-        test_class = Primitive(
+        test_class = self.Primitive(
             string=test_string,
             _float=test_float,
             integer=test_integer,
@@ -40,3 +38,7 @@ class TestPrimitiveModel(unittest.TestCase):
         self.assertEqual(test_class._float, test_float)
         self.assertEqual(test_class.integer, test_integer)
         self.assertEqual(test_class.boolean, test_boolean)
+
+
+if __name__ == '__main__':
+    unittest.main()
