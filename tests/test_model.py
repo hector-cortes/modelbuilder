523364a65dc18bc5d869dab810331bde1ffbedc2
Hector Cortes <hector.cortes@outlook.com>
2020-04-09 01:37:09 -0700

    additional test

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,17 +2,41 @@ import unittest
 
 from modelbuilder.model import Model, Attribute
 
-class TestSimpleModel(unittest.TestCase):
+
+class TestPrimitiveModel(unittest.TestCase):
     def test_model_creation(self):
         class Primitive(Model):
             string = Attribute(obj_name="string", obj_type=str)
             _float = Attribute(obj_name="float", obj_type=float)
             integer = Attribute(obj_name="integer", obj_type=int)
             boolean = Attribute(obj_name="boolean", obj_type=bool)
-        
+
         test_class = Primitive()
         self.assertIsNone(test_class.string)
         self.assertIsNone(test_class._float)
         self.assertIsNone(test_class.integer)
         self.assertIsNone(test_class.boolean)
 
+    def test_model_setget(self):
+        class Primitive(Model):
+            string = Attribute(obj_name="string", obj_type=str)
+            _float = Attribute(obj_name="float", obj_type=float)
+            integer = Attribute(obj_name="integer", obj_type=int)
+            boolean = Attribute(obj_name="boolean", obj_type=bool)
+
+        test_string = "Hello"
+        test_float = 1.0
+        test_integer = 5
+        test_boolean = True
+
+        test_class = Primitive(
+            string=test_string,
+            _float=test_float,
+            integer=test_integer,
+            boolean=test_boolean
+        )
+
+        self.assertEqual(test_class.string, test_string)
+        self.assertEqual(test_class._float, test_float)
+        self.assertEqual(test_class.integer, test_integer)
+        self.assertEqual(test_class.boolean, test_boolean)
