8479226941fc2d9964ae0753e8a8d536bb7683bf
Hector Cortes <hector.cortes@outlook.com>
2020-04-09 16:37:28 -0700

    more automerge fixes

--- automerge.sh
+++ automerge.sh
@@ -2,8 +2,8 @@ if [ "$TRAVIS_BRANCH" != "develop" ]; then
     exit 0;
 fi
 
-git config -- global user.name $GIT_NAME
-git config -- global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
 
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
